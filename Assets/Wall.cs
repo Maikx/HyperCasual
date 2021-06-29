0c613259bed84964e67ee0451b5a0e62b0366550
Maikx <mikeybee2000@gmail.com>
2021-06-29 16:17:40 +0200

    Added Simple Teleporting Walls

---
+++ Assets/Wall.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.transform.position.x > 0)
+            collision.gameObject.transform.position = new Vector3(-2.8f, collision.gameObject.transform.position.y, collision.gameObject.transform.position.z);
+        else
+            collision.gameObject.transform.position = new Vector3(2.8f, collision.gameObject.transform.position.y, collision.gameObject.transform.position.z);
+    }
+}
