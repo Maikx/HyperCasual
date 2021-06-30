39583cbaacfa00d79768a4aef5efc36aedec0752
Maikx <mikeybee2000@gmail.com>
2021-06-30 16:42:28 +0200

    Game Is Complete & Removed Unused Assets

---
+++ Assets/Scripts/Managers/LevelTransition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransition : MonoBehaviour
+{
+    public static LevelTransition self;
+
+    private void Start()
+    {
+        self = this;
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 20f;
+    private Rigidbody2D rb;
+
+    private float topScore = 0.0f;
+
+    public Text scoreText;
+
+    private float moveX;
+    private float accX;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>(); 
+    }
+
+    void Update()
+    {
+        if(rb.velocity.y > 0 && transform.position.y > topScore)
+        {
+            topScore = transform.position.y;
+        }
+
+        scoreText.text = "Score: " + Mathf.Round(topScore).ToString();
+
+        //moveX = Input.GetAxis("Horizontal") * moveSpeed;
+        accX = Input.acceleration.x * moveSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = accX;
+        //velocity.x = moveX;
+        rb.velocity = velocity;
+    }
+}
